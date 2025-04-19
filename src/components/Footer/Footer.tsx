72904bedf67818dbf74ba507378774ce8fd1087c
viilasa <viilasacontact@gmail.com>
2025-04-20 00:54:16 +0530

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -46,7 +46,7 @@ const Footer = () => {
               </li>
               <li className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-orange-500" />
-                <span className="text-gray-400">779 803 2404</span>
+                <span className="text-gray-400">+91 7972916974</span>
               </li>
               <li className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-orange-500" />
